455606400cd2f40b3888eb0d62118870e01c7b61
Muzammil <muzammil.c1990@gmail.com>
2026-09-02 10:38:15 +0530

    First playwright code

--- TheTestingAcademy/Playwright_Basics/tests/example.spec.ts
+++ TheTestingAcademy/Playwright_Basics/tests/example.spec.ts
@@ -1,18 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+/*test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
-});
+});*/
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('verify the title will be TTA cart', async ({ page }) => {
+  // await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // // Click the get started link.
+  // await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // // Expects page to have a heading with the name of Installation.
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+
+  await page.goto("https://app.thetestingacademy.com/playwright/ttacart/");
+  await expect(page).toHaveTitle("TTACart - Login");
+  await page.waitForTimeout(5000);
+
 });
